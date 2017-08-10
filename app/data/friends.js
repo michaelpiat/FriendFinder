ed45c380edee457555b9ff8fcfabc1a9ceee76df
Michael Piatnichko <mpiatnichko@gmail.com>
2017-08-10 14:36:36 -0600

    modal added

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 var friendsArray = [
 	{
 		name: "The Rock",
-		photo: "https://goo.gl/images/GAaJyD",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/The_ROCK.jpg/170px-The_ROCK.jpg",
 		scores: ["4","3","5","2","5","4","1","3","5","3"]
 	},
 	{
 		name: "Arwnald",
-		photo: "https://goo.gl/images/zQv3HT",
+		photo: "https://s-media-cache-ak0.pinimg.com/736x/54/5c/16/545c164f600b60969ad55425af21b6ed--classical-music-arnold-schwarzenegger.jpg",
 		scores: ["5","4","3","5","2","5","5","5","1","4"]
 	},
 	{
 		name: "Mia Wallace",
-		photo: "https://goo.gl/images/Xhm1zw",
+		photo: "http://dazedimg.dazedgroup.netdna-cdn.com/786/azure/dazed-prod/1140/5/1145937.jpg",
 		scores: ["3","5","1","4","4","3","3","3","4","5"]
 	}
 ];
