a90a274d43a89ceab61bcc19d855ce6c594ce45a
Michael Piatnichko <mpiatnichko@gmail.com>
2017-08-11 19:16:07 -0600

    server file change

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ require("./app/routing/htmlRoutes")(app);
 
 
 app.listen(port, function() {
-  console.log("App listening on PORT: " + PORT);
+  console.log("App listening on PORT: " + port);
 });
