6523cf1a472dfca27f1ce897734b6ade8a309d51
Michael Piatnichko <mpiatnichko@gmail.com>
2017-08-11 19:15:19 -0600

    server file fixed

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+var port = process.env.PORT || 3000;
+
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = 3000;
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,6 +18,6 @@ require("./app/routing/htmlRoutes")(app);
 
 
 
-app.listen(PORT, function() {
+app.listen(port, function() {
   console.log("App listening on PORT: " + PORT);
 });
